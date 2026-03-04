e479a458ede918dc9f8ad8780d0656c6722f3719
bera2003 <anuragbera7@gmail.com>
2026-03-04 21:16:23 +0530

    fix cors for production

--- Clinical_Backend/app/main.py
+++ Clinical_Backend/app/main.py
@@ -22,7 +22,10 @@ app.include_router(auth_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000"
+        "https://clinical-document-analysis.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
